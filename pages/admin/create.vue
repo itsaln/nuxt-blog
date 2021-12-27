354271363ccd961f66ca1c82838a20a3a4536fca
scripac <mukhammadiev013@gmail.com>
2021-12-28 00:26:22 +0500

    lesson 41 end frontend

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -31,6 +31,19 @@
       </div>
     </el-dialog>
 
+    <el-upload
+      class="mb"
+      drag
+      ref="upload"
+      action="https://jsonplaceholder.typecode.com/posts/"
+      :on-change="handleImageChange"
+      :auto-upload="false"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">Перетащите картинку <em>или нажмите</em></div>
+      <div class="el-upload__tip" slot="tip">файли с расширением jpg/png</div>
+    </el-upload>
+
     <el-form-item>
       <el-button
         type="primary"
@@ -49,6 +62,7 @@
     layout: 'admin',
     data() {
       return {
+        image: null,
         previewDialog: false,
         loading: false,
         controls: {
@@ -66,24 +80,32 @@
       }
     },
     methods: {
+      handleImageChange(file, fileList) {
+        this.image = file.raw
+      },
       onSubmit() {
         this.$refs.form.validate(async valid => {
-          if (valid) {
+          if (valid && this.image) {
             this.loading = true
 
             const formData = {
               title: this.controls.title,
-              text: this.controls.text
+              text: this.controls.text,
+              image: this.image
             }
 
             try {
               await this.$store.dispatch('post/create', formData)
               this.controls.title = ''
               this.controls.text = ''
+              this.image = null
+              this.$refs.upload.clearFiles()
               this.$message.success('Пост создан')
             } catch (e) {} finally {
               this.loading = false
             }
+          } else {
+            this.$message.warning('Форма не валидна')
           }
         })
       }
